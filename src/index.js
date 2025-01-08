b5fe742330f195e673c16825764655807d50e409
eko-prasetio <caedablerg@gmail.com>
2025-01-08 23:16:12 +0700

    callback payment midtrans

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import express from "express";
 import authRoutes from "./routes/authRoutes.js";
 import globalRoutes from "./routes/globalRoutes.js";
 import { connectDB } from "./utils/database.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
 const app = express();
 
 dotenv.config();
@@ -26,6 +27,7 @@ app.get("/", (req, res) => {
 
 app.use("/api", globalRoutes);
 app.use("/api", authRoutes);
+app.use("/api", paymentRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);

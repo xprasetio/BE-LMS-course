f95d31ebeb54770a516ab148b2b37b9c7152bd63
eko-prasetio <caedablerg@gmail.com>
2025-01-08 22:44:13 +0700

    setup midtrans

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,11 @@
 import bcrypt from "bcrypt";
+import TransactionModel from "../models/transactionModel.js";
 import userModel from "../models/userModel.js";
+
 export const signUpAction = async (req, res) => {
+  const midtransUrl = process.env.MIDTRANS_URL;
+  const midtransAuthString = process.env.MIDTRANS_AUTH_STRING;
+
   try {
     const body = req.body;
     const hashPassword = await bcrypt.hash(body.password, 10);
@@ -8,15 +13,45 @@ export const signUpAction = async (req, res) => {
     const user = new userModel({
       name: body.name,
       email: body.email,
-      photo: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+      photo: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
       password: body.password,
       role: "manager",
     });
+    const transaction = new TransactionModel({
+      user: user._id,
+      price: 100000,
+    });
+
+    const midtrans = await fetch(midtransUrl, {
+      method: "POST",
+      body: JSON.stringify({
+        transaction_details: {
+          order_id: transaction._id.toString(),
+          gross_amount: transaction.price,
+        },
+        credit_card: {
+          secure: true,
+        },
+        customer_details: {
+          email: user.email,
+        },
+        callbacks: {
+          finish: "http://localhost:5173/success-checkout",
+        },
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Basic ${midtransAuthString}`,
+      },
+    });
+    const midtransResponse = await midtrans.json();
     await user.save();
+    await transaction.save();
+    
     return res.json({
       message: "Sign up success",
       data: {
-        midtrans_payment_url: "https://app.sandbox.midtrans.com/snap/snapToken",
+        midtrans_payment_url: midtransResponse.redirect_url,
       },
     });
   } catch (error) {
--- src/models/transactionModel.js
+++ src/models/transactionModel.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+
 const transactionModel = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
